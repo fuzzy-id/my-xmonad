aeebc0cb2c68ea633d357d62768403d84c7dc01a
Thomas Bach <thbach@students.uni-mainz.de>
2015-01-20 00:56:57 +0100

    testing some basic properties of brightness functions at
    least ensure that functions work here

--- Test/Brightness.hs
+++ Test/Brightness.hs
@@ -1,8 +1,22 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Test.Brightness where
 
+import Test.Tasty (TestTree)
 import Test.Tasty.TH
 import Test.Tasty.HUnit
 
+import My.Brightness
+
+tests :: TestTree
 tests = $(testGroupGenerator)
 
+case_getBrightness_lies_in_borders :: Assertion
+case_getBrightness_lies_in_borders = do b <- getBrightness
+                                        minBrightness <= b @?= True
+                                        maxBrightness >= b @?= True
+
+case_setBrightness_to_getBrightness :: Assertion
+case_setBrightness_to_getBrightness = do bOld <- getBrightness
+                                         setBrightness bOld
+                                         bNew <- getBrightness
+                                         bOld @?= bNew
