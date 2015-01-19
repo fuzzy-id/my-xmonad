1bf7c56860436b082c2a59aeb4094ce401fabb46
Thomas Bach <thbach@students.uni-mainz.de>
2015-01-20 00:57:13 +0100

    new brightness tests

--- tests.hs
+++ tests.hs
@@ -3,9 +3,13 @@ module Main where
 
 import Test.Tasty
 
+import qualified Test.Brightness as TB
 import qualified Test.PidProg as TPD
 import qualified Test.Pulse as TP
 
 main :: IO ()
-main = defaultMain $ testGroup "Collected" [TPD.tests, TP.tests]
+main = defaultMain $ testGroup "Collected" [ TPD.tests
+                                           , TP.tests
+                                           , TB.tests
+                                           ]
 
